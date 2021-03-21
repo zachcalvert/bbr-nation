0e509bf87e2dcca070f735991b60adc1f72799b5
Zach Calvert <zcalvert@oreilly.com>
2021-03-21 13:36:48 -0700

    working detail modal AND working links

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -1,191 +1,33 @@
 import React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import axios from "axios"
-import { Backdrop, Dialog, Divider, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
-import { useSpring, animated } from 'react-spring/web.cjs';
+import { Divider, Typography } from '@material-ui/core';
 
 import { FormattedTime } from '../Common'
-import { API_URL } from "../../constants"
 import './Content.css';
 
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    overflow:'scroll',
-  },
-  modalPaper: {
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    height: "auto",
-    marginTop: theme.spacing(4)
-  },
-  avatar: {
-    height: 'auto',
-    width: '50px'
-  },
-  date: {
-    textAlign: 'right'
-  }
-}));
-
-const Fade = React.forwardRef(function Fade(props, ref) {
-  const { in: open, children, onEnter, onExited, ...other } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter();
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited();
-      }
-    },
-  });
-
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {children}
-    </animated.div>
-  );
-});
-
-Fade.propTypes = {
-  children: PropTypes.element,
-  in: PropTypes.bool.isRequired,
-  onEnter: PropTypes.func,
-  onExited: PropTypes.func,
-};
-
 export const Content = (props) => {
-  const location = useLocation();
   const { content } = props;
   const { kind } = content;
-  const classes = useStyles();
-  const history = useHistory();
-  const [conversation, setConversation] = React.useState([]);
-  const [open, setOpen] = React.useState(false);
-
-  const handleClose = () => {
-    setOpen(false);
-    const queryParams = new URLSearchParams(location.search);
-    if (queryParams.has('content')) {
-      queryParams.delete('content')
-      history.replace({
-        search: '',
-      })
-    }
-
-  };
-
-  React.useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const content = urlParams.get('content');
-    if (content) {
-      fetchConversation();
-      setOpen(true);
-    }
-  }, []);
-
-  const renderContent = (content) => {
-    return (
-      <>
-       <Typography variant='subtitle1'>{content.creator}</Typography>
-        {content.kind === 'IMAGE' && (
-          <div className='bbr-image'>
-            <img src={content.upload} />
-          </div>
-        )}
-        {content.kind === 'VIDEO' && (
-          <div className='bbr-video'>
-            <video controls><source src={content.upload} type="video/mp4" /></video>
-          </div>
-        )}
-        {content.text && <Typography variant='h4'>{content.text}</Typography>}
-        <Typography className={classes.date} variant='subtitle2'>{FormattedTime(content.create_date)}</Typography>
-      </>
-    )
-  }
-
-  async function fetchConversation() {
-    const { data } = await axios.get(`${API_URL}content/${content.name}/conversation`)
-    setConversation(data);
-  }
-
-  const handleClick = (e) => {
-    setOpen(true);
-    history.push({
-      search: `?content=${content.name}`
-    })
-    fetchConversation();
-    document.activeElement.blur();
-  };
 
   return (
     <div>
-      <Paper className={classes.paper} onClick={handleClick}>
-        {kind === 'IMAGE' && (
-            <div className='bbr-image'>
-            <img src={content.upload} />
-            {content.text && <Typography variant='h6'>"{content.text}"</Typography>}
-            </div>
-        )}
-        {kind === 'VIDEO' && (
-          <div className='bbr-video'>
-            <video controls><source src={content.upload} type="video/mp4" /></video>
+      {kind === 'IMAGE' && (
+          <div className='bbr-image'>
+          <img src={content.upload} />
+          {content.text && <Typography variant='h6'>"{content.text}"</Typography>}
           </div>
-        )}
-        {kind === 'TEXT' && (
-          <div className='bbr-quote'>
-            {content.text && <Typography variant='h6'>"{content.text}"</Typography>}
-          </div>
-        )}
-        <Divider />
-        <Typography variant='subtitle1'>{content.creator}, {FormattedTime(content.create_date)}</Typography>
-      </Paper>
-      <Dialog
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.modalPaper}>
-          {conversation.map((message, index) => (
-        <>
-        <Grid container>
-          <Grid item xs={2} ><img className={classes.avatar} src={message.avatar_url} /></Grid>
-          <Grid item xs={10} >
-            <Typography variant='subtitle1'>{message.creator}</Typography>
-            {message.text && <Typography variant='h6'>{message.text}</Typography>}
-          </Grid>
-        </Grid>
-        <Typography className={classes.date} variant='subtitle2'>{FormattedTime(message.created_date)}</Typography>
-        <Divider />
-        </>
-      ))}
-      {renderContent(content)}
-          </div>
-        </Fade>
-      </Dialog>
-      </div>
+      )}
+      {kind === 'VIDEO' && (
+        <div className='bbr-video'>
+          <video controls><source src={content.upload} type="video/mp4" /></video>
+        </div>
+      )}
+      {kind === 'TEXT' && (
+        <div className='bbr-quote'>
+          {content.text && <Typography variant='h6'>"{content.text}"</Typography>}
+        </div>
+      )}
+      <Divider />
+      <Typography variant='subtitle1'>{content.creator}, {FormattedTime(content.create_date)}</Typography>    
+    </div>
   )
 };
--- client/src/components/Page/Page.jsx
+++ client/src/components/Page/Page.jsx
@@ -1,19 +1,90 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import axios from "axios"
-import { API_URL } from "../../constants"
+import { Backdrop, Dialog, Divider, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { useSpring, animated } from 'react-spring/web.cjs';
 
+import { FormattedTime } from '../Common'
+import { API_URL } from "../../constants"
 import { Content } from '../Content/Content';
 import './Page.css'
 
 const FEED_URL = `${API_URL}content/`
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow:'scroll',
+  },
+  modalPaper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: "auto",
+    marginTop: theme.spacing(4)
+  },
+  avatar: {
+    height: 'auto',
+    width: '50px'
+  },
+  date: {
+    textAlign: 'right'
+  }
+}));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
+
 export const Page = () => {
   const { slug } = useParams();
-  const [content, setContent] = useState([]);
+  const history = useHistory();
+  const location = useLocation();
+  const classes = useStyles();
+
+  const [pageContent, setPageContent] = useState([]);
   const [isBottom, setIsBottom] = useState(false);
   const [nextUrl, setNexUrl] = useState(null);
   const [keepScrolling, setKeepScrolling] = useState(true);
+  const [conversation, setConversation] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [activeContent, setActiveContent] = React.useState(null);
 
   let contentUrl;
   if (slug) {
@@ -22,9 +93,29 @@ export const Page = () => {
     contentUrl = `${FEED_URL}random`;
   }
 
-  useEffect(() => {
-    setContent([]);
+  async function fetchPageContent(url) {
+    const { data } = await axios.get(url);
+    setPageContent([...pageContent, ...data.results]);
+    setIsBottom(false);
+    if (!data.next) {
+      setKeepScrolling(false);
+    }
+    setNexUrl(data.next);
+  }
 
+  async function fetchConversation(name) {
+    const { data } = await axios.get(`${API_URL}content/${name}/conversation`)
+    setConversation(data);
+  }
+
+  async function fetchContentDetails(name) {
+    const { data } = await axios.get(`${API_URL}content/${name}`);
+    setActiveContent(data);
+  }
+
+  // first load
+  useEffect(() => {
+    setPageContent([]);
     try {
       window.scroll({
         top: 0,
@@ -34,20 +125,19 @@ export const Page = () => {
     } catch (error) {
       window.scrollTo(0, 0);
     }
+    fetchPageContent(contentUrl);
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const contentName = urlParams.get('content');
 
-    async function fetchContent() {
-      const { data } = await axios.get(contentUrl);
-      setContent(data.results);
-      if (data.next) {
-        setNexUrl(data.next);
-      } else {
-        setKeepScrolling(false);
-      }
+    if (contentName) {
+      fetchContentDetails(contentName);
+      fetchConversation(contentName);
+      setOpen(true);
     }
-    fetchContent();
   }, [slug]);
 
-  // add scroll listener to detect if we've reach the bottom of the page
+  // scroll stuff
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
@@ -67,25 +157,69 @@ export const Page = () => {
 
   useEffect(() => {
     if (isBottom && keepScrolling) {
-      fetchContent(nextUrl);
+      fetchPageContent(nextUrl);
     }
   }, [isBottom]);
 
-  async function fetchContent(url) {
-    const { data } = await axios.get(url);
-    setContent([...content, ...data.results]);
-    setIsBottom(false);
-    if (!data.next) {
-      setKeepScrolling(false);
+
+  // modal stuff
+  const handleClose = () => {
+    setOpen(false);
+    const queryParams = new URLSearchParams(location.search);
+    if (queryParams.has('content')) {
+      queryParams.delete('content')
+      history.replace({
+        search: '',
+      })
     }
-    setNexUrl(data.next);
-  }
+  };
+
+  const handleClick = (content, e) => {
+    setOpen(true);
+    setActiveContent(content)
+    history.push({
+      search: `?content=${content.name}`
+    })
+    fetchConversation(content.name);
+    document.activeElement.blur();
+  };
 
   return (
     <>
-      {content.map((c, index) => (
-        <Content key={c.id} content={c} />
+      {pageContent.map((c) => (
+        <Paper className={classes.paper} onClick={(e) => handleClick(c, e)}>
+          <Content key={c.id} content={c} />
+        </Paper>
       ))}
+      <Dialog
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.modalPaper}>
+            {conversation.map((message, index) => (
+              <>
+                <Grid container>
+                  <Grid item xs={2}><img className={classes.avatar} src={message.avatar_url} /></Grid>
+                  <Grid item xs={10}>
+                    <Typography variant='subtitle1'>{message.creator}</Typography>
+                    {message.text && <Typography variant='h6'>{message.text}</Typography>}
+                  </Grid>
+                </Grid>
+                <Typography className={classes.date} variant='subtitle2'>{FormattedTime(message.created_date)}</Typography>
+                <Divider />
+              </>
+            ))}
+            {activeContent && <Content content={activeContent} />}
+          </div>
+        </Fade>
+      </Dialog>
     </>
   )
 }

a6a3a0a26f2c62a11c3addb2dbd3a4956310e545
Zach Calvert <zcalvert@oreilly.com>
2021-03-28 08:36:51 -0700

    Add readonly fields to admin

--- server/content/admin.py
+++ server/content/admin.py
@@ -7,6 +7,14 @@ class ContentAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'user', 'create_date', 'kind', 'upload', 'text']
     list_filter = ('user', 'kind',)
 
+    fields = (
+        'user',
+        'create_date',
+        'kind',
+        'upload',
+        'text',
+    )
+    readonly_fields = fields
 
 admin.site.register(Content, ContentAdmin)
 admin.site.register(Page)

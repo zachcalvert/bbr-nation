6c06bc8a8c5d9c8610f629a42ca07de34d3861d7
Zach Calvert <zcalvert@oreilly.com>
2021-04-13 12:45:53 -0700

    add autoPlay

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -18,7 +18,7 @@ export const Content = (props) => {
       )}
       {kind === 'VIDEO' && (
         <div className='bbr-video'>
-          <video controls loop muted playsInline><source src={content.upload} type="video/mp4" /></video>
+          <video controls autoPlay loop muted playsInline><source src={content.upload} type="video/mp4" /></video>
         </div>
       )}
       {kind === 'TEXT' && (

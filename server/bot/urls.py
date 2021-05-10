2cbddc79d1ac905d1147f79080a7cb9dde8f6ad6
Zach Calvert <zcalvert@oreilly.com>
2021-05-10 16:17:23 -0700

    update urls

--- server/bot/urls.py
+++ server/bot/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new_message/', views.NewMessageView.as_view(), name='new_message'),
+    path('new_message/', views.new_message, name='new_message'),
 ]

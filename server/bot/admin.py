77dc015ebd4d1af120aff1d9b56d4ac01915e2d8
Zach Calvert <zcalvert@oreilly.com>
2021-05-14 09:20:58 -0700

    add admin action to mark thoughts as unused

--- server/bot/admin.py
+++ server/bot/admin.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 from bot.models import GroupMeBot, Thought
 
 
+def mark_as_unused(modeladmin, request, queryset):
+    for thought in queryset:
+        thought.used = 0
+        thought.save()
+mark_as_unused.short_description = 'Mark as unused'
+
+
 class ThoughtAdmin(admin.ModelAdmin):
     list_display = ('text', 'approved', 'sentiment', 'member', 'player')
     list_filter = ('approved', 'used', 'sentiment', 'member', 'player')
+    actions = [mark_as_unused, ]
 
 
 admin.site.register(Thought, ThoughtAdmin)
